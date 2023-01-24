c377488ab2206a3694bb258b00d6a4b3e3a241ba
Konstantin Senchenkov <senchenkovko@gmail.com>
2023-01-24 22:59:41 +0400

    Exemple_005 Hello Maria

---
+++ Exemple/Exemple_005_NameBlock/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Print your name, please: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "Maria")
+{
+    Console.WriteLine("Yahho, Maria");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
